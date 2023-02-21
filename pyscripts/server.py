e224d9e9f7b98c6571864817c015f3a8a81d0b05
OopsOverflow <houssemmahmoud18@gmail.com>
2023-02-21 01:44:45 +0100

    Add video sampling with open cv

--- pyscripts/server.py
+++ pyscripts/server.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import numpy as np
 import cv2
 from tensorflow.keras.models import load_model
@@ -7,6 +9,7 @@ from tensorflow.keras.optimizers import Adam
 from flask import Flask, request, jsonify
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from flask_cors import CORS, cross_origin
+import tempfile
 
 
 # Load the pre-trained model in memory
@@ -19,6 +22,27 @@ def load_custom_model():
         print('Custom model not found, loading model_all.h5')
         model = load_model('model_all.h5', compile=False)
     return model
+
+
+def predict_emotion(model, frame):
+    # Load the model
+
+    # Preprocess the frame
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (48, 48))
+    img = np.expand_dims(img, axis=-1)
+    img = np.expand_dims(img, axis=0)
+    img = img.astype('float32') / 255.0
+
+    # Pass the frame through the pre-trained model to get the emotion
+    emotion = model.predict(img)[0]
+
+    return emotion
+
+
+def get_emotion_label(emotion):
+    emotion_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Neutral', 5: 'Sad', 6: 'Surprise'}
+    return emotion_dict[np.argmax(emotion)]
 
 
 # Define a Flask app object
@@ -41,8 +65,7 @@ def predict():
 
     # Pass the image through the pre-trained model to get the emotion
     emotion = model.predict(img)[0]
-    emotion_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Neutral', 5: 'Sad', 6: 'Surprise'}
-    emotion_label = emotion_dict[np.argmax(emotion)]
+    emotion_label = get_emotion_label(emotion)
 
     # Calculate the accuracy of the prediction
     accuracy = emotion[np.argmax(emotion)]
@@ -52,11 +75,82 @@ def predict():
     return jsonify(response)
 
 
+# Define a function to handle POST requests
+@app.route('/segment', methods=['POST'])
+@cross_origin(origin='*')
+def segment():
+    model = load_custom_model()
+
+    # Get the uploaded video file from the request
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+    video_file = request.files['file']
+
+    num_captures = 5
+
+    start = time.time()
+
+    # if captures exist in request, use that instead
+    if 'captures' in request.form:
+        num_captures = int(request.form['captures'])
+
+    try:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # Save the video file to disk
+            video_path = os.path.join(temp_dir, 'video.mp4')
+            video_file.save(video_path)
+
+            # Sample the frames and predict their emotions
+            cap = cv2.VideoCapture(video_path, cv2.CAP_FFMPEG)
+            num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            sample_interval = max(num_frames // num_captures, 1)
+            emotions = []
+            for i in range(num_frames):
+                # Set the frame position to sample
+                cap.set(cv2.CAP_PROP_POS_FRAMES, i * sample_interval)
+
+                # Read the frame
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                print('Predicting emotion for frame {}'.format(i))
+
+                # Pass the frame through the pre-trained model to get the emotion
+                emotion = predict_emotion(model, frame)
+
+                print('Emotion: {} - %: '.format(get_emotion_label(emotion)), emotion[np.argmax(emotion)])
+
+                emotions.append(emotion)
+
+            # Get the average emotion across all frames
+            emotions = np.array(emotions)
+            avg_emotion = np.mean(emotions, axis=0)
+
+            # Get the emotion label and accuracy
+            emotion_label = get_emotion_label(avg_emotion)
+            accuracy = avg_emotion[np.argmax(avg_emotion)]
+
+            end = time.time()
+
+            return jsonify({'emotion': emotion_label, 'accuracy': float(accuracy), 'time': (end - start)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    finally:
+        cap.release()
+
+        # release CUDA memory
+        import tensorflow as tf
+        tf.keras.backend.clear_session()
+
+
 @app.route('/retrain', methods=['POST'])
 @cross_origin(origin='*')
 def retrain():
     try:
-        model = load_model('model_all.h5')
+        model = load_custom_model()
 
         data = request.json['data']
 
