59b552180537e56b6b7cb133dd11514da8ccda47
Le-Roi-du-village <74618775+Le-Roi-du-village@users.noreply.github.com>
2023-02-23 01:19:16 +0100

    ajout image et temps + quelques modif de style

--- src/lib/marcelle/components/spotify-search/spotify-search.view.svelte
+++ src/lib/marcelle/components/spotify-search/spotify-search.view.svelte
@@ -33,8 +33,8 @@
 
 
     let input = '';
-    let searchResults: { id: string, name: string, artist: string }[] = [];
-    let activeResult: { id: string, name: string, artist: string } | null = null;
+    let searchResults: {id: string,name: string,artist: string,image: string,duration_ms: number}[] = [];
+    let activeResult: {id: string, name: string, artist: string, image: string,duration_ms: number } | null = null;
     async function handleSearch() {
       if (input.trim() === '') {
         searchResults = [];
@@ -47,6 +47,10 @@
           id: track.id,
           name: track.name,
           artist: track.artists[0].name,
+          image: track.album.images[0].url,
+          duration_ms: track.duration_ms
+
+
         }));
       } catch (error) {
         console.error(error);
@@ -58,74 +62,119 @@
         console.log(activeResult);
       }
     }
-  
+    function formatTime(ms: number) {
+        const minutes = Math.floor(ms / 60000);
+        const seconds = ((ms % 60000) / 1000).toFixed(0);
+        return `${minutes}:${(+seconds < 10 ? '0' : '')}${seconds}`;
+    }
     export let title: string;
   
   </script>
-  <style>
-    .search {
-      position: relative;
-      z-index: 1000;
-    }
+ <style>
+  .search {
+ position: relative;
+}
 
-    .search input[type="text"] {
-      width: 100%;
-      padding: 10px;
-      border: none;
-      border-bottom: 1px solid #ccc;
-      font-size: 16px;
-    }
-  
-    .search .results {
-      top: 100%;
-      left: 0;
-      right: 0;
-      max-height: 300px;
-      overflow-y: auto;
-      background: #fff;
-      border: 1px solid #ccc;
-      border-top: none;
-    }
-  
-    .search .results ul {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-    }
-  
-    .search .results li {
-      padding: 10px;
-      cursor: pointer;
-    }
-  
-    .search .results li:hover {
-      background: #f9f9f9;
-    }
-  
-    .search .results li.selected {
-      background: #f9f9f9;
-    } 
-  </style>
-  <ViewContainer {title}>
-      <div class="search">
-          <input type="text" placeholder="Rechercher sur Spotify" bind:value={input} on:input={handleSearch}>
-        
-          {#if searchResults.length > 0}
-            <div class="results">
-              <ul>
-                {#each searchResults as result}
-                  <li 
-                    on:click={() => {activeResult = result;console.log(activeResult)}} 
-                    class:selected={result === activeResult} 
-                    on:keydown={handleKeydown} 
-                    
-                  >
-                    {result.name} - {result.artist}
-                  </li>
-                {/each}
-              </ul>
-            </div>
-          {/if}
-        </div>
-  </ViewContainer>
-  
+.search input[type="text"] {
+ width: 100%;
+ padding: 10px;
+ border: none;
+ border-bottom: 1px solid #ccc;
+ font-size: 16px;
+}
+
+.search .results {
+ top: 100%;
+ left: 0;
+ right: 0;
+ max-height: 300px;
+ overflow-y: auto;
+ background: #fff;
+ border: 1px solid #ccc;
+ border-top: none;
+}
+
+.search .results ul {
+ margin: 0;
+ padding: 0;
+ list-style: none;
+}
+
+.search .results li {
+ padding: 10px;
+ cursor: pointer;
+ display: flex;
+ align-items: center;
+}
+
+.search .results li:hover {
+ background: #f9f9f9;
+}
+
+.search .results li.selected {
+ background: #f9f9f9;
+}
+
+.search .results li img {
+ margin-right: 10px;
+ height: 50px;
+ width: 50px;
+ object-fit: cover;
+}
+
+.search .results li .result-container {
+ display: flex;
+ flex-direction: row;
+ align-items: center;
+ width: 100%;
+}
+
+.search .results li .result-details {
+ display: flex;
+ flex-direction: column;
+ margin-left: 10px;
+ flex-grow: 1;
+}
+
+.search .results li .result-name {
+ font-weight: bold;
+}
+
+.search .results li .result-artist {
+ margin-top: 5px;
+ font-size: 14px;
+ color: #999;
+}
+
+.search .results li .result-time {
+ text-align: right;
+ font-size: 14px;
+ color: #999;
+ white-space: nowrap;
+}
+</style>
+<ViewContainer {title}>
+<div class="search">
+ <input type="text" placeholder="Rechercher sur Spotify" bind:value={input} on:input={handleSearch}>
+   
+ {#if searchResults.length > 0}
+   <div class="results">
+     <ul>
+       {#each searchResults as result}
+         <li on:click={() => { activeResult = result; }} class:selected={result === activeResult} on:keydown={handleKeydown} tabindex="0">
+           <div class="result-container">
+             <img src={result.image} alt={result.name}>
+             <div class="result-details">
+               <div class="result-name">{result.name}</div>
+               <div class="result-artist">{result.artist}</div>
+             </div>
+             
+             <div class="result-time">{formatTime(result.duration_ms)}</div>
+           </div>
+         </li>
+       {/each}
+     </ul>
+   </div>
+ {/if}
+</div>
+</ViewContainer>
